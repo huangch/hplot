afa78444c36fb6d6a8b7e161fdc78ff73ef97abf
huangc78 <chao-hui.huang@pfizer.com>
2025-09-13 13:34:37 -0700

    Update plotting.py

--- hplot/plotting.py
+++ hplot/plotting.py
@@ -1,20 +1,33 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.ticker import MaxNLocator
 
-def plot_hplot(grouped_stats, distance_col=None, distance_unit=None, ci_show=True, ax=None):
+def plot_hplot(grouped_stats, distance_unit=None, ci_show=True, ax=None):
     if ax is None:
         fig, ax = plt.subplots(figsize=(6, 4))
 
     for label, df in grouped_stats.items():
-        x = df['distance'] if distance_col else df['layer']
+        x = df['layer']
         y = df["mean"]
         ax.plot(x, y, label=str(label), drawstyle="steps-post")
 
         if ci_show:
             ax.fill_between(x, df["ci_lower"], df["ci_upper"], alpha=0.3, step="post")
 
-    ax.set_xlabel(f"Distance from tumor boundary ({distance_unit if distance_unit else 'Euclidean distance from border' if distance_col else 'layer index'})")  
-    ax.set_ylabel("Proportion of target cell type")
+    ax.ticklabel_format(axis='x', style='plain', useOffset=False)
+
+    def distance_formattyer(val, pos):
+        dst_list = []
+        for label, df in grouped_stats.items():
+            if val in df['layer']:
+                dst_list.append(df[df['layer']==val, 'distance'].values[0])
+            
+        dst_mean = np.mean(dst_list)
+
+        return f"{val:g}\n{dst_mean:.3f}" 
+    
+    ax.set_xlabel(f"Distance to tumor boundary (cellular layer index/distance{' ('+distance_unit+')' if distance_unit else ''})")  
+    ax.set_ylabel("Proportion of lymphocytes")
     ax.set_title("Spatial Heterogeneity Profile (H-Plot)")
     ax.legend(title="Group")
     ax.grid(True, linestyle="--", alpha=0.5)
