c6ea88ce89c3086cb8677270a8d260c273c72568
huangch <2898938+huangch@users.noreply.github.com>
2026-06-27 16:44:16 -0700

    modified:   hplot/plotting.py

--- hplot/plotting.py
+++ hplot/plotting.py
@@ -213,7 +213,6 @@ def plot_hplot(
                y,
                label=str(label),
                color=color,
-               drawstyle="steps-post",
                linewidth=2,
            )
            if ci_show:
@@ -227,7 +226,6 @@ def plot_hplot(
                    df["ci_upper"].to_numpy(),
                    color=color,
                    alpha=0.25,
-                   step="post",
                )
        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
        ax.set_ylabel(_build_ylabel(value_kind, display_target_type, ylabel))
